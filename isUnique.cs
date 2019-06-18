c8bf460331fc4e1316ae8c3c26e5f1ae72d8a712
nbonilla409 <48836196+nbonilla409@users.noreply.github.com>
2019-06-17 20:42:20 -0700

    Add files via upload

---
+++ isUnique.cs
@@ -0,0 +1,40 @@
+﻿//Implement an algorithm to determine if a string has all unique characters.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isUnique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isUnique("bol"));
+        }
+
+        public static bool isUnique(string input)
+        {
+            //if (input.Length > 128)
+            //    return false;
+
+            bool[] char_set = new bool[128];
+            for (int i = 0; i < input.Length; i++)
+            {
+                int val = input[i];
+                if (char_set[val])
+                    return false;
+                char_set[val] = true;
+            }
+            return true;
+
+            //var set = new HashSet<char>();
+            //for (int i = 0; i < input.Length; i++)
+            //    if (!set.Add(input[i]))
+            //        return false;
+            //return true;
+        }
+    }
+}
